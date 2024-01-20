8d36416afe7307312e2de669ad1b57cc6cc1bf7c
frosiky <a>
2024-01-20 22:05:59 +0900

    リセット機能を追加

--- src/app.vue
+++ src/app.vue
@@ -1,100 +1,83 @@
 <script setup>
-import disc from './component/disc.vue'
-import { ref } from 'vue'
-import Othello from './lib/Othello.js'
+import disc from "./component/disc.vue";
+import { ref } from "vue";
+import Othello from "./lib/Othello.js";
 
-const oth = new Othello()
-const board = ref(oth.board(true))
+let oth = new Othello();
+const board = ref(oth.board(true));
 //先手0 後手1
-const showPreview = true
-const pop = ref([2, 2])
-const preview = []
+const showPreview = true;
+const pop = ref([2, 2]);
+const state = ref("黒の手番です");
 
 const put = (index) => {
-  const move = 0x08000000000000000n >> BigInt(index)
-  const next = oth.putIf(oth.data[0], oth.data[1], move)
+  const move = 0x08000000000000000n >> BigInt(index);
+  const next = oth.putIf(oth.data[0], oth.data[1], move);
   if (next == 0) {
-    console.log('[Othello]Err: cannot place there')
-    return
+    console.log("[Othello]Err: cannot place there");
+    return;
   }
-  oth.put(move, next)
-  oth.next()
+  oth.put(move, next);
+  oth.next();
+  state.value = (oth.turn == 0 ? "黒" : "白") + "の手番です";
+  pop.value = oth.pop();
   if (oth.canput(oth.data, oth.turn) == 1) {
-    console.log('[Othello]: passed')
-    oth.log(0n, 0n)
-    oth.next()
+    console.log("[Othello]: passed");
+    oth.log(0n, 0n);
+    oth.next();
+    state.value =
+      "パスしました\n" + (oth.turn == 0 ? "黒" : "白") + "の手番です";
     if (oth.canput(oth.data, oth.turn) == 1) {
-      console.log('[Othello]: game ended')
-      oth.undo(1)
+      const res = pop.value[0] - pop.value[1];
+      state.value =
+        "ゲームセット\n" +
+        (res < 0 ? "黒の勝利" : res != 0 ? "白の勝利" : "引き分け") +
+        "です";
+      console.log("[Othello]: game ended");
+      oth.undo(1);
     }
   }
-  board.value = oth.board(showPreview)
-  pop.value = oth.pop()
-}
+  board.value = oth.board(showPreview);
+};
 const undo = () => {
-  oth.undo(1)
+  oth.undo(1);
+  board.value = oth.board(showPreview);
+  pop.value = oth.pop();
+};
+const reset = () => {
+  oth = new Othello();
   board.value = oth.board(showPreview)
   pop.value = oth.pop()
-}
+  state.value = "リセットしました 黒の手番です"
+};
 </script>
 
 <template>
   <div class="back">
     <div class="pop">
-      <disc color="0" class="color" />: {{ pop[0] }}　 <disc color="1" class="color" />:
+      <disc color="0" class="color" />: {{ pop[0] }}　
+      <disc color="1" class="color" />:
       {{ pop[1] }}
     </div>
+    <div class="state">{{ state }}</div>
     <table>
       <tr v-for="(line, i) in board.split(` `)">
         <td v-for="(cell, j) in line">
           <div @click="put(i * 8 + j / 1)">
-            <disc :color="cell == 2 ? (preview.includes([i, j]) ? 3 : 2) : cell" />
+            <disc :color="cell" />
           </div>
         </td>
       </tr>
     </table>
     <button @click="undo()">undo</button>
+    <button @click="reset()">reset</button>
   </div>
 </template>
 
 <style scoped>
-#play_button {
-  user-select: none;
-  cursor: pointer;
-  margin-top: 2%;
-  height: 15%;
-  right: 15%;
-  position: absolute;
-  display: flex;
-  border: 2px solid #3877ba;
-  border-radius: 100px;
-  padding-left: 10%;
-  padding-right: 10%;
-}
-#select {
-  width: 50%;
-  display: flex;
-  border-radius: 10px;
-}
-.selected {
-  background-color: #b7c1cd;
-}
-.cond {
-  user-select: none;
-  cursor: pointer;
-  border: #000 solid 1px;
-  border-radius: 10px;
-  text-align: center;
-  padding: 3%;
-  padding-top: 1%;
-  padding-bottom: 1%;
-  margin: auto;
-  margin-top: 1%;
-  margin-bottom: 1%;
-}
 .pop {
   display: flex;
-  font-size: 20px;
+  font-size: 4vh;
   margin-left: 1%;
   text-align: center;
   line-height: 100%;
@@ -107,6 +90,8 @@ const undo = () => {
   background-color: bisque;
   margin: 0;
   padding: 4vh;
+  max-width: 500px;
+  max-height: 500px;
 }
 td {
   border: solid 1.5px #000;
--- src/component/disc.vue
+++ src/component/disc.vue
@@ -6,7 +6,7 @@ const c = ["black", "white", "index"];
 <template>
   <div>
     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-      <circle v-if="[0, 1].includes(color / 1)" r="16" cx="16" cy="16" :fill="c[color]" />
+      <circle v-if="[0, 1].includes(color / 1)" r="16" cx="16" cy="16" :fill="c[color ^ 1]" />
       <circle v-if="color == 3" r="4" cx="16" cy="16" :fill="`red`" />
     </svg>
   </div>
